5cf21a911404c1328ed3a8543e5e34747fea246d
roblouie <rob.j.louie@gmail.com>
2016-03-13 15:53:04 -0500

    Facebook login 90% complete
    Server side handling of logging in with facebook.  The facebook login flow is designed to work with the existing email/password login flow such that once a user is logged in it doesn't matter if they logged in via email/password or via facebook.  This means that once we receive a token from facebook we then need to fetch that user's email and create a new user account with that email.  We then create a JWT token from that user just like we do for a normal login.
    
    The login flow for facebook works as follows:
    * In the client app (web, Android, iOS, etc), the user hits the "Login with Facebook" button.  This will give them an access token.  Our server side auth expects this token.
    * The client app posts their facebook token to our facebook auth endpoint
    * We first send this token to facebook's debug endpoint to get back the app id of the app that created the token. We compare the app id from the token with our app id and make sure they match.
    * We then exchange the short lived token for a long lived one.  Currently this information is only used to get the user information, but it can be stored to use later to make any facebook calls.
    * We get the user information from facebook: first name, last name, email, and facebook id
    * We use the email to look up a user in our database.  If a user is found return that user, otherwise create a user.  We put in a UUID as the password during user creation since the user will not be logging in with their password.
    * Use the found or newly created user to generate a JWT token.  The jwt token is returned in the response.  That token can now be used in exactly the same way as a JWT token generated from a username/password login
    
    TODO:
    * Store the long lived token in the database.  It's not needed for login, but if in the future the api is extended to make other facebook calls to get user data we will need the users tokens
    * Add facebook API version number to facebook config so it can be used when calling facebook endpoints
    * Compare user id from facebook token with user id sent in from client app.  This would be an additional step to ensure the token matched the user.

--- config/facebook-config.js
+++ config/facebook-config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    appID: '',
-    secret: ''
+    appID: '140878965998466',
+    secret: '1fe4de2fceccb84ffa6f67a0c823d247'
 };
---
+++ services/facebook-service.js
@@ -0,0 +1,78 @@
+'use strict';
+const facebookConfig = require('../config/facebook-config');
+const authService = require('./auth-service');
+const uuid = require('node-uuid');
+const Promise = require('bluebird');
+const url = require('url');
+const request = Promise.promisify(require("request"));
+const User = require('../models/user');
+const JWT = require('jwt-async');
+const securityConfig = require('../config/security-config');
+const jwt = Promise.promisifyAll(new JWT());
+Promise.promisifyAll(request);
+
+jwt.setSecret(securityConfig.jwtSecret);
+
+module.exports = {
+    loginWithFacebook
+};
+
+function loginWithFacebook(shortLivedToken) {
+    return doesAppIdInTokenMatchOurAppId(shortLivedToken)
+        .then(getLongLivedTokenFromFacebook)
+        .then(getUserInfoFromFacebook)
+        .then(findOrCreateUser)
+        .then(generateJwtToken)
+}
+
+function doesAppIdInTokenMatchOurAppId(shortLivedToken) {
+    return request(`https://graph.facebook.com/debug_token?input_token=${shortLivedToken}&access_token=${facebookConfig.appID}|${facebookConfig.secret}`)
+        .then(response => {
+            const responseJson = JSON.parse(response.body);
+            if (responseJson.error) throw new Promise.OperationalError(responseJson.error.message);
+            if (responseJson.data.app_id !== facebookConfig.appID) throw new Promise.OperationalError("App IDs do not match");
+            return shortLivedToken;
+        });
+}
+
+function getLongLivedTokenFromFacebook(shortLivedToken) {
+    return request(`https://graph.facebook.com/oauth/access_token?client_id=${facebookConfig.appID}&client_secret=${facebookConfig.secret}&grant_type=fb_exchange_token&fb_exchange_token=${shortLivedToken}`)
+        .then(response => {
+            //Parsing the response string is due to the fact that if there are no errors facebook responds to oauth/access_token
+            //with a query string-like format like so: access_token={long-term-token}&expires={expiration}
+            //By adding '?' to the start of that string it can be parsed as query string
+            const parsedResponseString = url.parse('?' + response.body, true).query;
+            return parsedResponseString.access_token;
+        });
+}
+
+function getUserInfoFromFacebook(longLivedToken) {
+    return request('https://graph.facebook.com/me?fields=email,first_name,last_name&access_token=' + longLivedToken)
+        .then(response => {
+            const responseJson = JSON.parse(response.body);
+            if (responseJson.error) throw new Promise.OperationalError(responseJson.error.message);
+            const userInfo = responseJson;
+            userInfo.longLivedToken = longLivedToken;
+            return userInfo;
+        });
+}
+
+function findOrCreateUser(userInfo) {
+    return User.where('username', userInfo.email).fetch()
+        .then(user => {
+            if (!user) {
+                //TODO: Save the long lived token to the user, it's not needed for auth but should be there for future fb calls,
+                //TODO: it is already on the userInfo object, it just needs saved to the database
+                return User.forge({
+                    username: userInfo.email,
+                    password: uuid.v1()
+                }).save()
+            }
+            return user
+        });
+}
+
+function generateJwtToken(user) {
+    return authService.generateToken(user);
+}
+
